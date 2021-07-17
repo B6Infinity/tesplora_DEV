5ebe8b7634ed35a8278b090adbd99d40f620e538
B6Infinity <broteendas.official@gmail.com>
2021-07-17 16:00:09 +0530

    3 Backend models made:- Profile, Gig, Image(not totally complete tho)

---
+++ DEVELOPER NOTES.md
@@ -0,0 +1,3 @@
+**NOTE:**
+
+1. All dummy users have the same password: `pass_123`
--- ground/views.py
+++ ground/views.py
@@ -1,13 +1,30 @@
-from django.http.response import HttpResponse
+from ground.models import Image
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
 
-    
+
     return render(request, 'home.html')
 
 def loginorsignup(request):
 
     return render(request, 'signuporlogin.html')
+
+
+# APIs
+
+def checkifusernameexists(request):
+    pass
+
+def handlesignup(request):
+    pass
+
+def handlelogin(request):
+    pass
+
+def handlelogout(request):
+    pass
+
